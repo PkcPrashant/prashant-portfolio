7c5a64d0ca3f8dc0a02de5b1ce4b65a772dffba2
Prashant Choudhary <pc22230@gmail.com>
2025-01-09 18:26:25 +0530

    navigation scrolling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,21 +5,23 @@ import sections from '../utility/sections';
 
 const Header = () => {
   return (
-    <Navbar collapseOnSelect expand="lg" fixed="top" data-bs-theme="dark" bg="black" >
-      <Container fluid>
-        <Navbar.Brand href="#home"><img src="/icon.ico" width="40" height="40" /></Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse>
-          <Nav className="ms-auto">
-            {
-              sections.map((section) => (
-                <Nav.Link href={`#${section?.href}`} key={section?.label}>{section?.label}</Nav.Link>
-              ))
-            }
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div className="header-card">
+      <Navbar collapseOnSelect expand="lg" fixed="top" data-bs-theme="dark" bg="black">
+        <Container fluid>
+          <Navbar.Brand href="#home"><img src="/icon.ico" width="40" height="40" /></Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse>
+            <Nav className="ms-auto">
+              {
+                sections.map((section) => (
+                  <Nav.Link href={`#${section?.href}`} key={section?.label}>{section?.label}</Nav.Link>
+                ))
+              }
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
   );
 }
 
